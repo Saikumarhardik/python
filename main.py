bb6de0d4f5e32eb3f44f8a19ae549f6efab876f8
Saikumarhardik <121499803+Saikumarhardik@users.noreply.github.com>
2023-06-12 22:34:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 
 os.system("top -bn 1 > saikumar.txt")
 
+li = []
+
 with open("saikumar.txt","r") as r:
   lines = r.readlines()
   for x in lines[2]:
-    y = x.strip().split(',')
-    print(y)
+    li.append(x.strip().split(','))
+print(li)
     
    
   
